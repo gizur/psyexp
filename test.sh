1c295f64fe2e91f0430e50eda3c13cc3500d3133
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2018-12-26 11:43:40 +0100

    Fix unit test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PORT=3000
-EXPERIMENT=32b3f330-0471-4dde-8f43-9282fc669ec2
+EXPERIMENT=10b096cd-0a74-49d9-b798-b3c22cf9b7dc
 TRIAL1=0e804680
 TRIAL2=27adb2d3
 
@@ -9,7 +9,7 @@ echo "Remove old test data:"
 s3cmd rm s3://psyexp/$EXPERIMENT/*
 
 echo -e "\nShuold say alive and well:"
-curl http://localhost:3000/status
+curl http://localhost:$PORT/status
 
 echo -e "\n\nTry to add an experiment, should get a uuid back:"
 curl -d '{"name": "My experiment", "email": "me@example.com"}' -X POST http://localhost:3000/add
